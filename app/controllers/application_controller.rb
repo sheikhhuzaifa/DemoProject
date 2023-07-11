0627922966c86443c6abf9044400a1af7ecfd1dd
sheikhhuzaifa <f190162@nu.edu.pk>
2023-07-11 13:05:54 +0500

    update devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :check_user_signed_in, unless: :devise_controller?
+
+  private
+
+  def check_user_signed_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
   def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [:username ,:firstname, :lastname])
 
